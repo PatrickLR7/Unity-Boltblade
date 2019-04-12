845ebafaf5e340300f46d9f351bb4b0c034555f5
Karman <krmanxv@gmail.com>
2019-04-11 19:46:20 -0600

    Shooting enemy

---
+++ 2D Side-Scroller - Boltblade/Assets/weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform FirePoint;
+    public GameObject bullet;
+
+    void Start()
+    {
+        InvokeRepeating("Shoot", 1.0f, 1.1f);
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, FirePoint.position, FirePoint.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
