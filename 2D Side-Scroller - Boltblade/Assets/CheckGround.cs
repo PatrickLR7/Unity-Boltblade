b2038ecf7446dd5c8f4bee68a7459d52aeff33bd
PatrickLR7 <patrickrx7@gmail.com>
2019-04-16 16:52:50 -0600

    Initial Animations: Idle, Walk and Jump.
    Initial Animations: Idle, Walk and Jump. It is necessary to polish them.

---
+++ 2D Side-Scroller - Boltblade/Assets/CheckGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private Player_controller player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<Player_controller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        player.grounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        player.grounded = false;
+    }
+}
--- 2D Side-Scroller - Boltblade/Assets/Player_controller.cs
+++ 2D Side-Scroller - Boltblade/Assets/Player_controller.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class Player_controller : MonoBehaviour
 {
-    public float speed = 25;
+    public float speed = 25f;
     public float maxSpeed = 5f;
     private Rigidbody2D playerRB2D;
     public float jumpPower = 1.5f;
     private bool jump;
     public float desiredx;
+    private Animator anim;
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-       
+        anim.SetFloat("speed", Mathf.Abs(playerRB2D.velocity.x));
+        anim.SetBool("grounded", grounded);
     }
 
-    //Trabajando con fisicas
+    //Should always be used for physics calculations.
     private void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
@@ -37,20 +40,17 @@ public class Player_controller : MonoBehaviour
             //playerRB2D.velocity.y = 6.5f;
         }
 
+        /*
         if (playerRB2D.velocity.x > maxSpeed) {
-
-           
-
-            playerRB2D.velocity = new Vector2(maxSpeed, playerRB2D.velocity.y);
-           
+            playerRB2D.velocity = new Vector2(maxSpeed, playerRB2D.velocity.y);   
         }
         else if (playerRB2D.velocity.x < -maxSpeed)
         {
-            playerRB2D.velocity = new Vector2(-maxSpeed, playerRB2D.velocity.y);
-
-           
+            playerRB2D.velocity = new Vector2(-maxSpeed, playerRB2D.velocity.y);     
         }
+        */
+        float limitedSpeed = Mathf.Clamp(playerRB2D.velocity.x, -maxSpeed, maxSpeed);
+        playerRB2D.velocity = new Vector2(limitedSpeed, playerRB2D.velocity.y);
 
-       
     }
 }
